75e41ef050d9749cf109d776b4de60eeda13ee3a
Charles Peterson <cpeterson@brokerbin.com>
2018-01-08 08:17:21 -0600

    keep system files in resources

--- system-after.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# If you would like to do some extra work on the host you may
-# add any commands you wish to this file and they will
-# be run after the Homestead machine is provisioned.
-
-echo "adding cert to trusted root certs"
-sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ca.homestead.crt
--- system-before.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# If you would like to do some extra work on the host you may
-# add any commands you wish to this file and they will
-# be run before the Homestead machine is provisioned.
