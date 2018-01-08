569d1c1f56a077c21eaceb69d47cd57d66557258
Charles Peterson <cpeterson@brokerbin.com>
2018-01-08 08:04:40 -0600

    before and after local host machine scripts

--- system-after.sh
+++ system-after.sh
@@ -4,8 +4,5 @@
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
 
-# https://github.com/laravel/homestead/pull/773
-# https://stackoverflow.com/questions/45263265/use-ssl-on-laravel-homestead
-# https://deliciousbrains.com/ssl-certificate-authority-for-local-https-development/
 echo "adding cert to trusted root certs"
 sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ca.homestead.crt
